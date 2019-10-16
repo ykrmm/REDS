45f03f87c2748c9c0a33cd40719ac4b2c33af869
4bach <yanniskarmim@hotmail.fr>
2019-10-16 19:14:50 +0200

    optim param

--- models/optim_params.py
+++ models/optim_params.py
@@ -50,36 +50,50 @@ if __name__ == "__main__":
                     score_opt_ams = score_ams
 
     l = {'models':'Random Forest','params':optim_param,'AMS':score_opt_ams}
-    optimal_parameters=optimal_parameters.append(l)          
+    optimal_parameters=optimal_parameters.append(l)  
+
+
     #Bagging 
-    score_min_bagg = 3.0
+    score_opt_ams = 0
     for bootstrap in [True,False]:
         for n_estimators in range(10,100,10):
             bagging = BaggingClassifier(n_estimators=n_estimators,bootstrap=bootstrap)
             bagging.fit(Xtrain,ytrain)
             ypred = bagging.predict(Xtest)
-            score = eval_.AMS(ytest,ypred,weights=weight_test)
-            if score >= score_min_bagg:
-                print(score)
-                bootstrap_opt = bootstrap
-                max_depth_opt = max_depth
-                n_estimators_opt = n_estimators 
-                score_min_bagg = score      
+            score_ams = eval_.AMS(ytest,ypred,weights=weight_test)
+            score_accuracy = eval_.accuracy(ytest,ypred)
+            score_precision = eval_.precision(ytest,ypred)
+            score_recall = eval_.recall(ytest,ypred)
+            l = {'models':'Bagging','params':str(rf.get_params()),\
+                'AMS':score_ams,'Accuracy':score_accuracy,'Precision':score_precision,'Recall':score_recall}
+            all_result_df=all_result_df.append(l,ignore_index=True)
+            if score_ams > score_opt_ams:
+                optim_param = str(rf.get_params())
+                score_opt_ams = score_ams
+
+    l = {'models':'Bagging','params':optim_param,'AMS':score_opt_ams}
+    optimal_parameters=optimal_parameters.append(l)     
                 
     #Adaboost
-    score_min_ab = 3.0
+    score_opt_ams = 0
     for n_estimators in [10, 30, 50, 100, 200, 400, 600, 800, 1000]:
         for learning_rate in range(0.1,1.1,0.1):
             adaboost = AdaBoostClassifier(n_estimators=n_estimators,learning_rate=learning_rate)
             adaboost.fit(Xtrain,ytrain)
             ypred = adaboost.predict(Xtest)
-            score = eval_.AMS(ytest,ypred,weights=weight_test)
-            if score >= score_min_ab:
-                print(score)
-                bootstrap_opt = bootstrap
-                max_depth_opt = max_depth
-                n_estimators_opt = n_estimators 
-                score_min_ab = score
+            score_ams = eval_.AMS(ytest,ypred,weights=weight_test)
+            score_accuracy = eval_.accuracy(ytest,ypred)
+            score_precision = eval_.precision(ytest,ypred)
+            score_recall = eval_.recall(ytest,ypred)
+            l = {'models':'AdaBoost','params':str(rf.get_params()),\
+                'AMS':score_ams,'Accuracy':score_accuracy,'Precision':score_precision,'Recall':score_recall}
+            all_result_df=all_result_df.append(l,ignore_index=True)
+            if score_ams > score_opt_ams:
+                optim_param = str(rf.get_params())
+                score_opt_ams = score_ams
+
+    l = {'models':'AdaBoost','params':optim_param,'AMS':score_opt_ams}
+    optimal_parameters=optimal_parameters.append(l) 
 
     dataset = Dataset(drop_weight=True)
     higgs_df = dataset.get_df()
@@ -87,16 +101,34 @@ if __name__ == "__main__":
     weight_train,weight_test = dataset.get_weight_train_test()
     eval_ = Evaluation() 
 
-    params = {
-        'min_child_weight': [1, 5, 10],
-        'gamma': [0.5, 1, 1.5, 2, 5],
-        'subsample': [0.6, 0.8, 1.0],
-        'colsample_bytree': [0.6, 0.8, 1.0],
-        'max_depth': [3, 4, 5]
-        }
     #XGBoost 
-    score_min_xg = 3.0
-    #c koa ce truk
+    score_opt_ams = 0
+
+    for min_child_weight in [1,5,10]:
+        for gamma in [0.5, 1, 1.5, 2, 5]:
+            for subsample in [0.6, 0.8, 1.0]:
+                for colsample_bytree in [0.6, 0.8, 1.0]:
+                    for max_depth in [3, 4, 5]:
+                        xgb = XGBClassifier(min_child_weight=min_child_weight,max_depth=max_depth,gamma=gamma,subsample=subsample,colsample_bytree=colsample_bytree)
+                        adaboost = AdaBoostClassifier(n_estimators=n_estimators,learning_rate=learning_rate)
+                        adaboost.fit(Xtrain,ytrain)
+                        ypred = adaboost.predict(Xtest)
+                        score_ams = eval_.AMS(ytest,ypred,weights=weight_test)
+                        score_accuracy = eval_.accuracy(ytest,ypred)
+                        score_precision = eval_.precision(ytest,ypred)
+                        score_recall = eval_.recall(ytest,ypred)
+                        l = {'models':'XGBoost','params':str(rf.get_params()),\
+                            'AMS':score_ams,'Accuracy':score_accuracy,'Precision':score_precision,'Recall':score_recall}
+                        all_result_df=all_result_df.append(l,ignore_index=True)
+                        if score_ams > score_opt_ams:
+                            optim_param = str(rf.get_params())
+                            score_opt_ams = score_ams
+
+    l = {'models':'XGBoost','params':optim_param,'AMS':score_opt_ams}
+    optimal_parameters=optimal_parameters.append(l) 
+    
+    optimal_parameters.to_csv("optimal_parameters.csv",index=False)
+    all_result_df.to_csv("all_result.csv")
                     
 
 
